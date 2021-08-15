31977bc6018c8e2e83147824ba9c3b136a5057da
cordelia <80014084+cordelia-sixth@users.noreply.github.com>
2021-08-15 23:47:24 +0900

    Update A_Counting.cpp

--- ABC209/A_Counting.cpp
+++ ABC209/A_Counting.cpp
@@ -5,6 +5,11 @@ int main() {
   int a, b;
   cin >> a >> b;
   cout << max(0, (b - a + 1)) << endl;
+  
+  // int a[3];
+  // for(int i = 0; i < 3; i++) cin >> a[i];
+  // sort(a, a + 3);
+  // cout << a[1] + a[2] << endl;
  
   return 0;
 }
